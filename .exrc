d1e69a7a1401b5219605ccc623ace6a1ef61077c
datwaft <datwaftx@gmail.com>
2023-03-02 19:55:19 -0600

    feat: use xorshift128+ to generate random numbers

--- .exrc
+++ .exrc
@@ -1,3 +1,4 @@
 augroup local_readonly_options
   autocmd BufRead compile_commands.json set readonly nospell norelativenumber
+  autocmd BufRead src/deps/*.c set readonly nospell norelativenumber
 augroup END
--- src/main.c
+++ src/main.c
@@ -2,13 +2,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "deps/xorshift128plus.c"
+
 int main(int argc, char **argv) {
+  s[0] = 69;
+  s[1] = 420;
+
   for (int c = 'A'; c < 'A' + 26; c++) {
     // Colors
     // 31 = RED, 36 = CYAN
-    int color = rand() % (36 - 31 + 1) + 31;
+    int color = next() % (36 - 31 + 1) + 31;
     // Styles
-    int style = rand() % (7 + 1);
+    int style = next() % (7 + 1);
     printf("\x1b[%d;%dm"
            "%c"
            "\x1b[0m",
