e09ae29401fef37c61ea9df7f83e8a79fa6da140
Michael <michaelyipchenmz@gmail.com>
2023-03-19 14:14:57 -0600

    feat: generic test progress bar

--- lottery-scheduler-gui/main.c
+++ lottery-scheduler-gui/main.c
@@ -3,6 +3,8 @@
 
 #define GLADE_FILE "glade/lottery-scheduler-gui.glade"
 
+int valor = 0;
+
 typedef struct gui_t {
   GtkWidget *window_main;
   GtkWidget *button_execute;
@@ -11,17 +13,18 @@ typedef struct gui_t {
   GtkWidget *label_quantum_or_percentage;
   GtkWidget *entry_quantum_or_percentage;
   GtkWidget *label_unit_quantum_or_percentage;
+  GtkWidget *generic_progress_bar;
 } gui_t;
 
 static void activate(GtkApplication *app, gui_t gui, gpointer user_data);
-
-void on_button1_clicked(GtkWidget *widget, gpointer data);
 
 void on_button_execute_clicked(GtkWidget *widget, gpointer data);
 
 void on_changed(GtkComboBox *widget, gpointer data);
 
 static void destroy(GtkWidget *widget, gpointer data);
+
+void scheduler_stub();
 
 /***********************************************************************************************************************/
 static void activate(GtkApplication *app, gui_t gui, gpointer user_data) {
@@ -50,6 +53,9 @@ static void activate(GtkApplication *app, gui_t gui, gpointer user_data) {
   gui.label_unit_quantum_or_percentage = GTK_WIDGET(
       gtk_builder_get_object(builder, "label_unit_quantum_or_percentage"));
 
+  gui.generic_progress_bar =
+      GTK_WIDGET(gtk_builder_get_object(builder, "generic_progress_bar"));
+
   /* Connects */
   gtk_builder_connect_signals(builder, NULL);
 
@@ -74,6 +80,11 @@ void on_button_execute_clicked(GtkWidget *widget, gpointer data) {
   entry_text = gtk_entry_get_text(GTK_ENTRY(gui->entry_thread_num));
 
   g_print("%s\n", entry_text);
+
+  gtk_progress_bar_set_fraction(GTK_PROGRESS_BAR(gui->generic_progress_bar),
+                                0.1);
+
+  // scheduler_stub();
 }
 
 void on_changed(GtkComboBox *widget, gpointer data) {
@@ -83,13 +94,15 @@ void on_changed(GtkComboBox *widget, gpointer data) {
   switch (gtk_combo_box_get_active(GTK_COMBO_BOX(gui->cb_operation_mode))) {
   case 0:
     g_print("You chose %s\n", "0");
-    gtk_label_set_text(GTK_LABEL(gui->label_quantum_or_percentage), "Tamaño Quantum");
+    gtk_label_set_text(GTK_LABEL(gui->label_quantum_or_percentage),
+                       "Tamaño Quantum");
     gtk_label_set_text(GTK_LABEL(gui->label_unit_quantum_or_percentage), "ms");
     break;
   case 1:
     g_print("You chose %s\n", "1");
-        g_print("You chose %s\n", "0");
-    gtk_label_set_text(GTK_LABEL(gui->label_quantum_or_percentage), "Trabajo Mínimo");
+    g_print("You chose %s\n", "0");
+    gtk_label_set_text(GTK_LABEL(gui->label_quantum_or_percentage),
+                       "Trabajo Mínimo");
     gtk_label_set_text(GTK_LABEL(gui->label_unit_quantum_or_percentage), "\%");
     break;
   default:
@@ -99,6 +112,13 @@ void on_changed(GtkComboBox *widget, gpointer data) {
 }
 
 static void destroy(GtkWidget *widget, gpointer data) { gtk_main_quit(); }
+
+void scheduler_stub() {
+  for (int i = 0; i < 10; i++) {
+    g_print("%d\n", i);
+    sleep(1);
+  }
+}
 
 int main(int argc, char **argv) {
   GtkApplication *app;
