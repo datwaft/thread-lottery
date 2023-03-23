d654a183534f17d7ab48d66d887643c793b597bc
Michael <michaelyipchenmz@gmail.com>
2023-03-23 17:44:24 -0600

    feat: added dynamic creation of thread configuration item box from value selected or changed of thread num spinbutton

--- lottery-scheduler-gui/main-v2.c
+++ lottery-scheduler-gui/main-v2.c
@@ -7,6 +7,13 @@ int valor = 0;
 
 typedef struct gui_t {
   float progress;
+  int min_ticket;
+  int max_ticket;
+  int min_work;
+  int max_work;
+  int default_ticket;
+  int default_work;
+  int default_thread_num;
   GtkWidget *window_main;
   GtkWidget *button_execute;
   GtkWidget *spin_thread_num;
@@ -16,7 +23,8 @@ typedef struct gui_t {
   GtkWidget *entry_quantum_or_percentage;
   GtkWidget *label_unit;
   GtkWidget *generic_progress_bar;
-  GtkWidget *box_thread_config, *view_thread_config;
+  GtkWidget *box_thread_config;
+  GtkWidget *view_thread_config;
 
 } gui_t;
 
@@ -28,7 +36,11 @@ void on_changed(GtkComboBox *widget, gpointer data);
 
 static void destroy(GtkWidget *widget, gpointer data);
 
-void scheduler_stub();
+void on_changed_sbtn_thread_num(GtkComboBox *widget, gpointer data);
+
+void generate_thread_conf_row(int threads_num, gpointer data);
+
+void clear_thread_conf_row(gpointer data);
 
 /***********************************************************************************************************************/
 static void activate(GtkApplication *app, gui_t gui, gpointer user_data) {
@@ -36,6 +48,17 @@ static void activate(GtkApplication *app, gui_t gui, gpointer user_data) {
   GtkWidget *window_main;
 
   gui.progress = 0.0;
+  // Tickets default, min, max values of ticket and work
+  gui.min_ticket = 0;
+  gui.max_ticket = 1000;
+
+  gui.min_work = 0;
+  gui.max_work = 1000;
+
+  gui.default_ticket = 5;
+  gui.default_work = 200;
+
+  gui.default_thread_num = 5;
 
   /* Import UI designed via Glade */
   builder = gtk_builder_new();
@@ -63,16 +86,30 @@ static void activate(GtkApplication *app, gui_t gui, gpointer user_data) {
   gui.box_thread_config =
       GTK_WIDGET(gtk_builder_get_object(builder, "box_thread_config"));
 
-  int min_ticket = 0;
-  int max_ticket = 1000;
+  generate_thread_conf_row(gui.default_thread_num, &gui);
 
-  int min_work = 0;
-  int max_work = 1000;
+  /* Connect components signal to functions */
+  gtk_builder_connect_signals(builder, NULL);
 
-  int default_ticket = 5;
-  int default_work = 200;
+  g_signal_connect(window_main, "destroy", G_CALLBACK(destroy), &gui);
 
-  int threads_num = 3;
+  g_signal_connect(gui.button_execute, "clicked",
+                   G_CALLBACK(on_button_execute_clicked), &gui);
+
+  g_signal_connect(gui.cb_operation_mode, "changed", G_CALLBACK(on_changed),
+                   &gui);
+
+  g_signal_connect(gui.spin_thread_num, "value-changed",
+                   G_CALLBACK(on_changed_sbtn_thread_num), &gui);
+
+  g_object_unref(builder);
+  gtk_widget_show(window_main);
+  gtk_main();
+}
+
+void generate_thread_conf_row(int threads_num, gpointer data) {
+  gui_t *gui;
+  gui = (gui_t *)data;
 
   for (int i = 0; i < threads_num; i++) {
     // create one grid_row per row, 3 columns
@@ -86,12 +123,12 @@ static void activate(GtkApplication *app, gui_t gui, gpointer user_data) {
     label_thread = gtk_label_new(str_thread_number);
 
     GtkAdjustment *adjustment_ticket;
-    adjustment_ticket = gtk_adjustment_new(default_ticket, min_ticket,
-                                           max_ticket, 1.0, 5.0, 0.0);
+    adjustment_ticket = gtk_adjustment_new(gui->default_ticket, gui->min_ticket,
+                                           gui->max_ticket, 1.0, 5.0, 0.0);
 
     GtkAdjustment *adjustment_work;
-    adjustment_work =
-        gtk_adjustment_new(default_work, min_work, max_work, 1.0, 5.0, 0.0);
+    adjustment_work = gtk_adjustment_new(gui->default_work, gui->min_work,
+                                         gui->max_work, 1.0, 5.0, 0.0);
 
     GtkWidget *sbtn_ticket;
     sbtn_ticket = gtk_spin_button_new(adjustment_ticket, 1.0, 0);
@@ -110,25 +147,40 @@ static void activate(GtkApplication *app, gui_t gui, gpointer user_data) {
     gtk_grid_set_column_homogeneous(GTK_GRID(grid_row), TRUE);
     gtk_grid_set_column_spacing(GTK_GRID(grid_row), 3);
 
-    gtk_box_pack_start(GTK_BOX(gui.box_thread_config), grid_row, FALSE, FALSE,
+    gtk_box_pack_start(GTK_BOX(gui->box_thread_config), grid_row, FALSE, FALSE,
                        3);
 
     gtk_widget_show(sbtn_ticket);
     gtk_widget_show(sbtn_work);
     gtk_widget_show(label_thread);
   }
+}
 
-  /* Connects */
-  gtk_builder_connect_signals(builder, NULL);
+void clear_thread_conf_row(gpointer data) {
+  gui_t *gui;
+  gui = (gui_t *)data;
 
-  g_signal_connect(window_main, "destroy", G_CALLBACK(destroy), &gui);
+  GList *children =
+      gtk_container_get_children(GTK_CONTAINER(gui->box_thread_config));
 
-  g_signal_connect(gui.button_execute, "clicked",
-                   G_CALLBACK(on_button_execute_clicked), &gui);
+  while (children) {
+    gtk_container_remove(GTK_CONTAINER(gui->box_thread_config), children->data);
+    children = children->next;
+  }
+}
 
-  g_object_unref(builder);
-  gtk_widget_show(window_main);
-  gtk_main();
+void on_changed_sbtn_thread_num(GtkComboBox *widget, gpointer data) {
+  gui_t *gui;
+  gui = (gui_t *)data;
+
+  gint threads_to_generate =
+      gtk_spin_button_get_value(GTK_SPIN_BUTTON(gui->spin_thread_num));
+  g_print("Thread to generate: %i\n", threads_to_generate);
+
+  // remove all children
+  clear_thread_conf_row(data);
+
+  generate_thread_conf_row(threads_to_generate, data);
 }
 
 void on_button_execute_clicked(GtkWidget *widget, gpointer data) {
@@ -141,30 +193,30 @@ void on_button_execute_clicked(GtkWidget *widget, gpointer data) {
   // gtk_progress_bar_set_fraction(GTK_PROGRESS_BAR(gui->generic_progress_bar),
   //                               i);
 
-  g_print("You chose %i\n", val);
+  g_print("Threads configurations %i\n", val);
 
   GList *children =
       gtk_container_get_children(GTK_CONTAINER(gui->box_thread_config));
 
+  // get generated values of the thread conf box
   while (children) {
-    // we can start creating task here 
+    // we can start creating task here
     gint ticket_column_value = gtk_spin_button_get_value(
         GTK_SPIN_BUTTON(gtk_grid_get_child_at(GTK_GRID(children->data), 1, 0)));
 
     gint work_column_value = gtk_spin_button_get_value(
         GTK_SPIN_BUTTON(gtk_grid_get_child_at(GTK_GRID(children->data), 2, 0)));
 
-    g_print("%i - %i\n", ticket_column_value, work_column_value);
+    g_print("Tickets: %i | Work: %i\n", ticket_column_value, work_column_value);
 
     children = children->next;
   }
-  
-
 }
 
 void on_changed(GtkComboBox *widget, gpointer data) {
-  gui_t *gui;
-  gui = (gui_t *)data;
+  g_print("You chose %s\n", "help am changed");
+  // gui_t *gui;
+  // gui = (gui_t *)data;
 
   // switch (gtk_combo_box_get_active(GTK_COMBO_BOX(gui->cb_operation_mode))) {
   // case 0:
@@ -186,23 +238,25 @@ void on_changed(GtkComboBox *widget, gpointer data) {
   // }
 }
 
-static void destroy(GtkWidget *widget, gpointer data) { gtk_main_quit(); }
-
-void scheduler_stub() {
-  for (int i = 0; i < 10; i++) {
-    g_print("%d\n", i);
-    sleep(1);
-  }
+static void destroy(GtkWidget *widget, gpointer data) {
+  g_print("You chose %i\n", 333333);
+  gtk_main_quit();
 }
 
 int main(int argc, char **argv) {
   GtkApplication *app;
+
   int status = 0;
+
   /* Initialize the environment */
   app = gtk_application_new("com.soa.lottery.scheduler",
                             G_APPLICATION_FLAGS_NONE);
+
   g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
+
   status = g_application_run(G_APPLICATION(app), argc, argv);
+
+  g_print("You chose %i\n", 333333);
   g_object_unref(app);
 
   return status;
