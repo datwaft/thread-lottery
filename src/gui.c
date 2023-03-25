b6484b372453149f18d137af1e38ddd15745d017
datwaft <datwaftx@gmail.com>
2023-03-25 01:28:01 -0600

    refactor: use gtk_builder_new_from_resource

--- src/gui.c
+++ src/gui.c
@@ -9,17 +9,11 @@ GtkApplication *application_new(void) {
 }
 
 void application_on_activate(GtkApplication *app, gpointer user_data) {
-  GtkBuilder *builder = gtk_builder_new();
-
-  GError *error = NULL;
-  if (!gtk_builder_add_from_resource(builder, TEMPLATE_URI, &error)) {
-    g_printerr("%s\n", error->message);
-    exit(EXIT_FAILURE);
-  }
-
+  GtkBuilder *builder = gtk_builder_new_from_resource(TEMPLATE_URI);
   GtkWidget *window =
       GTK_WIDGET(gtk_builder_get_object(builder, "window_main"));
   gtk_builder_connect_signals(builder, NULL);
+
   gtk_widget_show_all(window);
   gtk_main();
 }
